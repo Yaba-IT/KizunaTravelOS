4dffe8f75d4efa7bce16b7a62257b601d4d81f5f
Farid <farid.hammouten@gmail.com>
2025-07-05 23:09:08 +0200

    chore(lint): add root ESLint + Prettier configs

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+    browser: false,
+  },
+  parserOptions: {
+    ecmaVersion: 2023,
+    sourceType: 'module',
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended', 
+  ],
+  plugins: ['import', 'jest', 'prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'error',
+  },  
+  overrides: [
+    {
+      files: ['apps/web/**/*.{js,jsx}'],
+      env: { browser: true },
+      extends: [
+        'airbnb',
+        'airbnb/hooks',
+        'plugin:react/jsx-runtime',
+        'plugin:prettier/recommended',
+      ],
+      settings: {
+        react: { version: 'detect' },
+      },
+      rules: {
+        'react/react-in-jsx-scope': 'off', 
+      },
+    },
+  ],
+};
