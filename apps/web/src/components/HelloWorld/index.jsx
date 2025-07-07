c2886dd5aa524dedda6b26b395859d01cbe37a7d
Farid <farid.hammouten@gmail.com>
2025-07-07 18:28:12 +0200

    components: HelloWorld +jest test file

---
+++ apps/web/src/components/HelloWorld/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+function HelloWorld (props) {
+
+  return (
+    <div>Hello World</div>
+  )
+}
+
+HelloWorld.propTypes = {
+  props: PropTypes.any
+}
+
+export default HelloWorld
