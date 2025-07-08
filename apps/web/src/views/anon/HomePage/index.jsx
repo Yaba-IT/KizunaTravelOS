cf8b428d6fbd65ecb300fbe65978774550bd87ee
Jules Loïc <julesloic9@gmail.com>
2025-07-08 20:59:33 +0200

    installation pakage for mui icon

--- apps/web/src/views/anon/HomePage/index.jsx
+++ apps/web/src/views/anon/HomePage/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import HelloWorld from '../../../components/HelloWorld';
+import MenuComponent from '../../../components/NavBar';
+import { AppBar, Box, Container, Toolbar } from '@mui/material';
+import AdbIcon from '@mui/icons-material/Adb';
+import NavBar from '../../../components/NavBar';
 
 function HomePage() {
   return (
     <>
+      <NavBar />
       <h1>Home Page</h1>
       <HelloWorld />
+
     </>
   );
 }
