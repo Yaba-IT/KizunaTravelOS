32a4234212d27e6e2343abdab25643f3a99831fc
Jules Loïc <julesloic9@gmail.com>
2025-07-10 12:45:00 +0200

    navigation button item componant

---
+++ apps/web/src/components/NavBtnItem/index.jsx
@@ -0,0 +1,114 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import {Button, Box} from '@mui/material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { IconButton, Typography } from '@mui/material';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import PropTypes from 'prop-types';
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color: 'rgb(55, 65, 81)',
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+    ...theme.applyStyles('dark', {
+      color: theme.palette.grey[300],
+    }),
+  },
+}));
+
+function NavBtnItem({ name, target, sub }, ...props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Button
+        variant="contained"
+        disableElevation
+        sx={{ my: 2 }}
+        onClick={()=>console.log(`Navigating to ${target}`)}
+        {...props}
+        endIcon={ sub &&
+          <Box data-testid="btn-menu" onClick={handleClick} color='inherit' display={'flex'} alignItems={'center' }>
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </Box>
+        }
+      >
+        <Typography variant='body1'>
+          {name}
+        </Typography>
+        
+      </Button>
+      {sub && (
+          <StyledMenu
+            slotProps={{
+              list: {
+                'aria-labelledby': 'scrollBtn',
+              },
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+          >
+            {
+              sub?.map((item) => (
+                <MenuItem onClick={handleClose} disableRipple>
+                  {item?.name ?? "na"}
+                </MenuItem>
+              ))
+            }
+          </StyledMenu>
+        )}
+    </>
+  );
+}
+
+NavBtnItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  target: PropTypes.string, 
+  sub: PropTypes.arr
+}
+
+export default NavBtnItem;
