79e2576bef1d4e409f5d9cd1eb257b348fdc130d
Jules Loïc <julesloic9@gmail.com>
2025-07-10 12:40:33 +0200

    Component : navigation bar

--- apps/web/src/components/NavBar/index.jsx
+++ apps/web/src/components/NavBar/index.jsx
@@ -1,46 +1,23 @@
-import { useState } from 'react';
+import React from 'react';
 import {
   AppBar,
   Box,
-  Toolbar,
-  IconButton,
-  Typography,
-  Menu,
   Container,
-  Avatar,
-  Button,
-  Tooltip,
-  MenuItem,
 } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
 import AdbIcon from '@mui/icons-material/Adb';
 import NavBtn from '../NavBtn';
 
-
-const pages = ['Products', 'Pricing', 'Blog'];
-
 function NavBar() {
-  const [anchorElNav, setAnchorElNav] = useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
   return (
-    <>
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Box disableGutters sx={{ display: "flex", w: 100, flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-            <AdbIcon />
-            <NavBtn />
-          </Box>
-        </Container>
-      </AppBar>
-    </>);
+    <AppBar data-testid="app-bar" position="static">
+      <Container maxWidth="xl">
+        <Box disableGutters sx={{ display: "flex", w: 100, flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+          <AdbIcon />
+          <NavBtn />
+        </Box>
+      </Container>
+    </AppBar>
+  );
 }
 
 export default NavBar;
