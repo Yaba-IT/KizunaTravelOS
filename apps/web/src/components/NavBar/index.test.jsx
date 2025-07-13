b6b7eb7bd9f21dd96c4050df7cd40a763bdc9e1e
Jules Loïc <julesloic9@gmail.com>
2025-07-13 03:03:30 +0200

    code refactoring

--- apps/web/src/components/NavBar/index.test.jsx
+++ apps/web/src/components/NavBar/index.test.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { cleanup, render, screen } from "@testing-library/react";
-import NavBtn from ".";
+import NavBar from ".";
 
 describe('NavBar', () => {
     afterEach(cleanup);
     it('renders the NavBar with NavBtn', () => {
-        render(<NavBtn />);
+        render(<NavBar />);
         const appBar = screen.getByTestId('app-bar');
         expect(appBar).toBeInTheDocument();
         const button = screen.getByRole("button", { name: "Home" });
