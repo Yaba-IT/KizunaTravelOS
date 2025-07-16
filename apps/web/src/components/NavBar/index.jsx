a17b5e0034502e3368d351306034af3a35d1608a
Jules Loïc <julesloic9@gmail.com>
2025-07-16 04:46:51 +0200

    Make the Navigation bar dinamic

--- apps/web/src/components/NavBar/index.jsx
+++ apps/web/src/components/NavBar/index.jsx
@@ -7,15 +7,15 @@ import {
   Typography,
 } from '@mui/material';
 import NavBtn from '../NavBtn';
-import logo from '../../assets/logo.png'
+import logo from '/src/assets/img/logo.png'
 
-function NavBar() {
+function NavBar({links}) {
   return (
     <AppBar data-testid="app-bar" position="static" sx={{ backgroundColor: "#152e44"}}>
       <Container maxWidth="xl">
         <Box sx={{ display: "flex", w: 100, flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
           <Box component={"img"} src={logo} height={65} alt={`${logo}-image`} />
-          <NavBtn />
+          <NavBtn links={links} />
           <Button
             variant="contained"
             sx={{ display: { xs: "none", md: "block" }, borderRadius: 10, textTransform: "capitalize", px: 3, py: 1, backgroundColor: "#f4bb3a", color: "white" }}
