71626779280ec81ef2f9d2e259d10654b62ee9c4
Jules Loïc <julesloic9@gmail.com>
2025-07-16 04:47:45 +0200

    fix the unit test form the Navigation bar

--- apps/web/src/components/NavBar/index.test.jsx
+++ apps/web/src/components/NavBar/index.test.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import { cleanup, render, screen } from "@testing-library/react";
 import NavBar from ".";
 
+jest.mock('../../assets/img/logo.png', () => 'logo.png');
+
+const links = [
+    { name: "Home", sub: null },
+    { name: "Service", sub: [{ name: "cloud" }] },
+    { name: "Contact us", sub: null },
+];
 describe('NavBar', () => {
     afterEach(cleanup);
     it('renders the NavBar with NavBtn', () => {
-        render(<NavBar />);
+        render(<NavBar links={links} />);
         const appBar = screen.getByTestId('app-bar');
         expect(appBar).toBeInTheDocument();
         const button = screen.getByRole("button", { name: "Home" });
