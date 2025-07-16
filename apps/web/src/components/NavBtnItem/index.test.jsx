ad3097a233c84f9cc81200958188807a6e966973
Jules Loïc <julesloic9@gmail.com>
2025-07-16 04:55:03 +0200

    update unit test for navigation button items

--- apps/web/src/components/NavBtnItem/index.test.jsx
+++ apps/web/src/components/NavBtnItem/index.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { cleanup, render, screen, fireEvent } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import NavBtnItem from ".";
 
 
@@ -11,30 +11,4 @@ describe("NavBtnItem", () => {
 		const button = screen.getByText("Home");
 		expect(button).toBeInTheDocument();
 	});
-
-	// it("renders a button with a sub-menu when sub-items are present", () => {
-	// 	render(<NavBtnItem name={elements.name} target={elements.target} sub={elements.sub} />);
-	// 	const button = screen.getByRole("button", { name: element.name });
-	// 	expect(button).toBeInTheDocument();
-	// });
-
-	// it("does not render a sub-menu when no sub-items are present", () => {
-	// 	render(<NavBtnItem name={elements.name} target={elements.target} sub={elements.sub} />);
-	// 	const subMenu = screen.queryByRole("menu");
-	// 	expect(subMenu).not.toBeInTheDocument();
-	// });
-
-	// it("render a sub-menu when sub-items are present", () => {
-	// 	render(<NavBtnItem name={elements.name} target={elements.target} sub={elements.sub} />);
-	// 	const subMenuBtn = screen.getByTestId("btn-menu");
-	// 	expect(subMenuBtn).toBeInTheDocument();
-	// });
-
-	// it("toggles the sub-menu visibility when the button is clicked", () => {
-	// 	render(<NavBtnItem name={elements.name} target={elements.target} sub={elements.sub} />);
-	// 	const subMenuBtn = screen.getByTestId("btn-menu");
-	// 	fireEvent.click(subMenuBtn);
-	// 	const subMenu = screen.getByRole("menu");
-	// 	expect(subMenu).toBeVisible();
-	// });
 });
