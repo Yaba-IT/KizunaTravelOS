61ed19b62cc4943f437221f52c1cbd41eca4f8b9
Jules Loïc <julesloic9@gmail.com>
2025-07-19 14:22:47 +0200

    navigation links table

---
+++ apps/web/src/constants/navbarItemsLists.js
@@ -0,0 +1,37 @@
+const navbarItemsLists = {
+    logo: "/src/assets/img/logo.png",
+    navigationElements: [
+        {
+            name: "Home",
+            target: "/",
+            sub: null
+        },
+        {
+            name: "Product",
+            target: "/products",
+            sub: [
+                {
+                    name: "blog",
+                    target: "/products/blog"
+                }
+            ]
+        },
+        {
+            name: "Service",
+            target: "http://service.com",
+            sub: [
+                {
+                    name: "cloud",
+                    target: "/service/cloud"
+                }
+            ]
+        },
+        {
+            name: "Contact us",
+            target: "/contact",
+            sub: null
+        }
+    ]
+}
+
+export default navbarItemsLists;
