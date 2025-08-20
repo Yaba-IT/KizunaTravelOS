1a004a831eea60893a8b4489580792c424fa1655
Farid <farid.hammouten@gmail.com>
2025-08-20 10:39:23 +0200

    update

---
+++ apps/web/src/contants/AnonNavbarItems.js
@@ -0,0 +1,70 @@
+const AnonNavbarItems = [
+  {
+    name: "navbar.about.name",
+    target: "#about",
+    url: null,
+    sub: [
+      {
+        name: "navbar.about.sub.us.name",
+        target: "/about/team",
+        url: null,
+        sub: null,
+      },
+      {
+        name: "navbar.about.sub.app.name",
+        target: "/about/kizuna",
+        url: null,
+        sub: null,
+      },
+    ],
+  },
+  {
+    name: "navbar.features.name",
+    target: "/features",
+    url: null,
+    sub: [
+      {
+        name: "navbar.features.sub.journey",
+        target: "/features/journey",
+        url: null,
+        sub: null,
+      },
+      {
+        name: "navbar.features.sub.guide",
+        target: "/features/guides",
+        url: null,
+        sub: null,
+      },
+      {
+        name: "navbar.features.sub.booking",
+        target: "/features/booking",
+        url: null,
+        sub: null,
+      },
+      {
+        name: "navbar.features.sub.customer",
+        target: "/features/cutomer",
+        url: null,
+        sub: null,
+      },
+      {
+        name: "navbar.features.sub.roleAndPermissions",
+        target: "/features/role-and-permissions",
+        url: null,
+        sub: null,
+      },
+    ],
+  },
+  {
+    name: "navbar.price.name",
+    target: "#price",
+    url: null,
+    sub: null,
+  },
+  {
+    name: "navbar.faq.name",
+    target: "#faq",
+    url: null,
+    sub: null,
+  },
+]
